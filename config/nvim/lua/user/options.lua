c7b507055248f18c942ab3d834b8168f26deb1b6
Daniel Wang <103014684+cloudyapple4@users.noreply.github.com>
2024-12-25 00:31:17 +1100

    add nvim config

---
+++ config/nvim/lua/user/options.lua
@@ -0,0 +1,29 @@
+local set = vim.opt
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+set.smartindent = true
+set.wrap = false
+set.number = true
+set.relativenumber = true
+set.mouse = "a"
+set.showmode = false
+set.clipboard = "unnamedplus"
+set.undofile = true
+set.ignorecase = true
+set.smartcase = true
+set.signcolumn = "yes"
+set.updatetime = 250
+set.timeoutlen = 300
+set.splitright = true
+set.splitbelow = true
+set.list = true
+set.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+set.cursorline = true
+set.scrolloff = 10
+set.hlsearch = true
---
+++ config/nvim/lua/user/plugins/init.lua
@@ -0,0 +1,16 @@
+return {
+    { "numToStr/Comment.nvim", opts = {} },
+
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signs = {
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+            },
+        },
+    },
+}
