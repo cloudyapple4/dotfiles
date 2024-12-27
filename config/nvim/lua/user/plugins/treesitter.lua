f5bd0511aa5a6e61b2901da8bf99c4e1ad5361b1
Daniel Wang <103014684+cloudyapple4@users.noreply.github.com>
2024-12-27 19:58:32 +1100

    nvim: cleanup treesitter

--- config/nvim/lua/user/plugins/treesitter.lua
+++ config/nvim/lua/user/plugins/treesitter.lua
@@ -1,18 +1,21 @@
-return     {
+return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    opts = {
-        ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
-        auto_install = true,
+    config = function()
+    require'nvim-treesitter.configs'.setup {
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        auto_install = false,
         highlight = {
             enable = true,
-            additional_vim_regex_highlighting = { "ruby", "python" },
+            disable = function(lang, buf)
+                local max_filesize = 100 * 1024 -- 100 KB
+                local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+            end,
+            additional_vim_regex_highlighting = false,
         },
-        indent = { enable = true, disable = { "ruby", "python" } },
-    },
-    config = function(_, opts)
-        require("nvim-treesitter.install").prefer_git = true
-        ---@diagnostic disable-next-line: missing-fields
-        require("nvim-treesitter.configs").setup(opts)
+    }
     end,
 }
